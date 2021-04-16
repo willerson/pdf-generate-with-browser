877d3bbc109fdee73d5ce03deeaf00d50b083674
Will <willerson.miranda@gmail.com>
2021-04-15 22:35:42 -0300

    finalizado a parte de gerar o pdf

--- src/server.js
+++ src/server.js
@@ -1,58 +1,77 @@
-const express = require('express')
-const ejs = require('ejs')
-const path = require('path')
-const pdf = require('html-pdf')
-const app = express()
+const express = require("express");
+const ejs = require("ejs");
+const path = require("path");
+const puppeteer = require('puppeteer');
+const app = express();
 
 const passengers = [
-    {
-        name: "Joyce",
-        flightNumber: 7859,
-        time: "18h00",
-    }, 
-    {
-        name: "Brock",
-        flightNumber: 7859,
-        time: "18h00",  
-    },
-    {
-        name: "Eve",
-        flightNumber: 7859,
-        time: "18h00",
-    }
+  {
+    name: "Joyce",
+    flightNumber: 7859,
+    time: "18h00",
+  },
+  {
+    name: "Brock",
+    flightNumber: 7859,
+    time: "18h00",
+  },
+  {
+    name: "Eve",
+    flightNumber: 7859,
+    time: "18h00",
+  },
 ];
+// ASYNC - retorna uma promise (promessa). O código só continua quando retornar um valor, a promese é resolvida com o valor retornado
+// Await - função assíncrona pode ter await. O await pausa a execusão da função e espera a resposta da promise
+// endpoint para pegar os dados do documento print-ejs
+// request = pedido
+// response = reposta
+app.get('/pdf', async (request, response) => {
+
+    // variável que inicia o browser - por padrão o { headless: false }
+    const browser = await puppeteer.launch()
+    // variável que abre uma nova página no browser
+    const page = await browser.newPage()
+
+    // local onde o pdf será aberto
+    await page.goto('http://localhost:3000/', {
+      // esperar carregar toda a página
+      waitUntil: 'networkidle0'
+    })
+
+    // seta a formatação do pdf que será gerado
+    const pdf = await page.pdf({
+      printBackground: true,
+      format: 'Letter',
+      padding: {
+        top: "20px",
+        bottom: "40px",
+        left: "20px",
+        right: "20px"
+      }
+    })
+
+    // fecha o browser
+    await browser.close()
+
+    
+    response.contentType("application/pdf")
+    
+    return response.send(pdf)
+})
 
 // request = pedido
 // response = reposta
-app.get('/', (request, response) => {
+app.get("/", (request, response) => {
+  const filePath = path.join(__dirname, "print.ejs");
+  ejs.renderFile(filePath, { passengers }, (err, html) => {
+    if (err) {
+      return response.send("Erro na leitura do arquivo");
+    }
 
-        const filePath = path.join(__dirname, "print.ejs")
-        ejs.renderFile(filePath, { passengers }, (err, html) => {
-            if(err) {
-                return response.send('Erro na leitura do arquivo')
-            }
+    // enviar para o navegador
+    return response.send(html);
+  });
+});
 
-            const options = {
-                height: "11.25in",
-                width: "8.5in",
-                header: {
-                    height: "20mm"
-                },
-                footer: {
-                    height: "20mm"
-                }
-            }
-
-            // criar o pdf
-            pdf.create(html, options).toFile("report.pdf", (err, data) => {
-                if(err) {
-                    return response.send("Erro ao gerar o PDF")
-                }
-
-                // enviar para o navegador
-                return response.send(html)
-            })
-        })
-})
-
-app.listen(3000)
+app.listen(3000);
